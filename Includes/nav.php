75ed6b9b49a9d4a476914b2f002b180c7c1ccd6a
Ameer-HamzaButt <44523981+Ameer-HamzaButt@users.noreply.github.com>
2019-01-21 20:01:07 +0500

    Templates

---
+++ Includes/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-dark">
+    <a class="navbar-brand" href="#">Steam Accounts Pakistan</a>
+    <!-- Links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Registration.html">Registration</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Login.html">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Forget%20Password.html">Forget Password</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Contact%20Us.html">Contact</a>
+        </li>
+    </ul>
+
+</nav>
