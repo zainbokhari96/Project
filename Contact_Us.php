28d0e4d4c06abe8b0f88b502497c8978b1ffadcf
Ameer-HamzaButt <44523981+Ameer-HamzaButt@users.noreply.github.com>
2019-01-21 17:45:38 +0500

    Update Contact_Us.php

--- Contact_Us.php
+++ Contact_Us.php
@@ -127,42 +127,44 @@ if(isset($_POST['insert_contact_us'])){
                 <div class="mapouter"><div class="gmap_canvas"><iframe width="300" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=university%20of%20central%20punjab%20lahore&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.pureblack.de">pureblack.de</a></div><style>.mapouter{text-align:right;height:200px;width:300px;}.gmap_canvas {overflow:hidden;background:none!important;height:200px;width:300px;}</style></div>            </div>
             </div>
     </div>
+<div id="footer">
+    <footer class="ct-u-backgroundDarkGray" style="margin-bottom: 40px">
 
-<footer class="ct-u-backgroundDarkGray" style="margin-bottom: 40px">
-
-    <div class="ct-footerBottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <p>COPYRIGHT © <a href="/">Steam Accounts Pakistan</a> 2018</p>
-                </div>
-                <div class="col-sm-6">
-                    <div class="pull-right">
-                        <div class="ct-socials">
-                            <ul class="Allign-the-elements">
-                                <li>
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest"><i class="fa fa-fw fa-pinterest"></i></a>
-                                </li>
-                                <li>
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram"><i class="fa fa-fw fa-instagram"></i></a>
-                                </li>
-                                <li>
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook"><i class="fa fa-fw fa-facebook-square"></i></a>
-                                </li>
-                                <li>
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fa fa-fw fa-twitter"></i></a>
-                                </li>
-                                <li>
-                                    <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="LinkedIn"><i class="fa fa-fw fa-linkedin"></i></a>
-                                </li>
-                            </ul>
+        <div class="ct-footerBottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p>COPYRIGHT © <a href="/">Steam Accounts Pakistan</a> 2018</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="pull-right">
+                            <div class="ct-socials">
+                                <ul class="Allign-the-elements">
+                                    <li>
+                                        <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest"><i class="fa fa-fw fa-pinterest"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram"><i class="fa fa-fw fa-instagram"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook"><i class="fa fa-fw fa-facebook-square"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fa fa-fw fa-twitter"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="LinkedIn"><i class="fa fa-fw fa-linkedin"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</div>
+
 
 </body>
 </html>
