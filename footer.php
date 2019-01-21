95a0f18dad3a9802979b0f449cef45a750eee734
Ameer-HamzaButt <44523981+Ameer-HamzaButt@users.noreply.github.com>
2019-01-21 16:16:50 +0500

    Added Header and Footer Files

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+<footer class="ct-u-backgroundDarkGray" style="margin-bottom: 40px">
+
+          <div class="ct-footerBottom">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-sm-6">
+                          <p>COPYRIGHT © <a href="/">Steam Accounts Pakistan</a> 2018</p>
+                      </div>
+                      <div class="col-sm-6">
+                          <div class="pull-right">
+                              <div class="ct-socials">
+                                  <ul class="Allign-the-elements">
+                                      <li>
+                                          <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Pinterest"><i class="fa fa-fw fa-pinterest"></i></a>
+                                      </li>
+                                      <li>
+                                          <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram"><i class="fa fa-fw fa-instagram"></i></a>
+                                      </li>
+                                      <li>
+                                          <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook"><i class="fa fa-fw fa-facebook-square"></i></a>
+                                      </li>
+                                      <li>
+                                          <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter"><i class="fa fa-fw fa-twitter"></i></a>
+                                      </li>
+                                      <li>
+                                          <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="LinkedIn"><i class="fa fa-fw fa-linkedin"></i></a>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </footer>
+?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="We sell Steam gaming accounts all over Pakistan">
+    <meta name="author" content="Steam Accounts Pakistan">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>About</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
+    <link
+            href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css"
+            rel="stylesheet"  type='text/css'>
+    <link  rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- <link  rel="stylesheet" type="text/css" href="css/style.css">-->
+     <link rel="stylesheet" href="css/style.css">
+
+
+ </head>
+?>
